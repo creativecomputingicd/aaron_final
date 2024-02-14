f01d36522a7bf22e41810bbb92fb6f9e3893ddba
Zuardin Akbar <akbar.zuardin@gmail.com>
2024-02-14 18:59:09 +0100

    Update Vite Config 2

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: "/aaron_final/",
-  build: {
-    rollupOptions: {
-        input: ['index.html', 'app.html', 'howItWorks.html'],
-        output: {
-            entryFileNames: ['index.html', 'app.html', 'howItWorks.html']
-        }
-    },
-},
 });
